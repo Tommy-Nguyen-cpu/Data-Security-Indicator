04407d25752c85f6d082e2dcac6f0a6b62edb012
Tony Ly <73499945+TonyLy-Git@users.noreply.github.com>
2023-03-20 18:54:38 -0400

    made global variable sites
    which holds the list of websites used by user

--- Data Security Indicator/App.xaml.cs
+++ Data Security Indicator/App.xaml.cs
@@ -5,13 +5,20 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Data_Security_Indicator
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+
+    public enum Websites
+    {
+        FACEBOOK, AMAZON, TIKTOK, LINKEDIN, SNAPCHAT, TWITTER, YOUTUBE, PINTEREST
+    }
     public partial class App : Application
     {
+        public static List<Websites> sites= new List<Websites>();
     }
 }
